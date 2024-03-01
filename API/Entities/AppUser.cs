0d764b33a8d38b67644253d8babe7328377f2ade
Chirag Sharma <chiragsharmaofficials@gmail.com>
2024-03-02 00:49:29 +0530

    Added validation and authentication using JWT (JSON Web Token)

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,8 +9,10 @@ namespace API.Entities
     {
 
         public int Id { get; set; }
-        public string? UserName { get; set; }
-
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+  
         // public int Id { get; set; }
         // public required string UserName { get; set; }
         // private int myVar;
